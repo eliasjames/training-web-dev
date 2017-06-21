7bb9c0c95c1730a38ed870dc420b13f1582f2029
Elias Carlston <elias@eliascarlston.com>
2017-06-21 10:45:33 -0400

    Working MVP for CLI TTT

--- intro-to-web-dev/examples/day2/02-js/cli-tictactoe/main.js
+++ intro-to-web-dev/examples/day2/02-js/cli-tictactoe/main.js
@@ -2,6 +2,8 @@ let currentPlayer;
 let movePromptObj = {
   'playerone': 'Player one, row and column',
   'playertwo': 'Player two, row and column',
+  'winone': 'Player one wins',
+  'wintwo': 'Player two wins',
   'badinput': 'Bad input, try again'
 };
 let moveStorage = [];
@@ -13,11 +15,45 @@ function acceptInput( moveCoords ) {
   if ( !moveCoords || moveCoords.length != 3 || typeof convertCoordsToArray( moveCoords ) !== 'number' ) {
     throw movePromptObj[ 'badinput' ];  
   }
-  storeInput( convertCoordsToArray( moveCoords ));
+  gameCycle( convertCoordsToArray( moveCoords ));
+}
+function checkWin() {
+  let winConditions = [
+    [ 0, 1, 2 ],
+    [ 3, 4, 5 ],
+    [ 6, 7, 8 ],
+    [ 0, 3, 6 ],
+    [ 1, 4, 7 ],
+    [ 2, 5, 8 ],
+    [ 0, 4, 8 ],
+    [ 2, 4, 6 ]
+  ], 
+    win = false,
+    result;
+
+  for (let i=0; i < winConditions.length; i++ ) {
+    result = '';
+    winConditions[ i ].forEach( function( eachIndex ) {
+      result += moveStorage[ eachIndex ];
+      if ( result === 'xxx' || result === 'ooo' ) {
+        win = true;
+        // stop looping
+        ( win ) ? i = winConditions.length : undefined; 
+      }
+    });
+  }
+  return win;
+}
+function changePlayer() {
+  getOrChangePlayer( true );
+}
+function convertCoordsToArray( moveCoords ) {
+  let splitCoords = moveCoords.split( '' );
+  return parseInt( splitCoords[0] ) * 3 + parseInt ( splitCoords[2] );
 }
 function getOrChangePlayer( change ) {
-  if ( !currentPlayer ) { 
-    currentPlayer = false;
+  if ( currentPlayer === undefined ) { 
+    currentPlayer = true;
   }
   if ( change ) {
     currentPlayer = !currentPlayer;
@@ -34,20 +70,25 @@ function getCurrentPlayer() {
       'name'  : 'two'
     };
 }
-function convertCoordsToArray( moveCoords ) {
-  let splitCoords = moveCoords.split( '' );
-  return parseInt( splitCoords[0] ) + parseInt ( splitCoords[2] );
-}
 function promptInput( promptMessageKey ) {
-  let currentPlayer = getCurrentPlayer();
-  let specificPrompt = promptMessageKey + currentPlayer.name;
+  let player = getCurrentPlayer();
+  let specificPrompt = promptMessageKey + player.name;
   let result = window.prompt( movePromptObj[ specificPrompt ], '0x0');
   acceptInput( result );
 }
 function storeInput( moveCoord ) {
   let player = getCurrentPlayer();
-  moveStorage[ moveCoord ] = player;
-  getOrChangePlayer( true );
+  moveStorage[ moveCoord ] = player.letter;
+}
+function gameCycle( moveCoord ) {
+  storeInput( moveCoord );
+
   console.log( 'keedl ms', moveStorage );
-  promptInput( 'player' );
+
+  if ( !checkWin() ) {
+    changePlayer( true );
+    promptInput( 'player' );
+    return;
+  }
+  alert( movePromptObj[ 'win' + getCurrentPlayer().name ] );
 }
