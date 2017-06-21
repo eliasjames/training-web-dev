6017d32df1d0c174de560ae785c83fd6e4881126
Elias Carlston <elias@eliascarlston.com>
2017-06-21 15:00:15 -0400

    Add DOM example

---
+++ intro-to-web-dev/examples/day3/pm-js/DOM/01-player-constructor.js
@@ -0,0 +1,28 @@
+function Player( newObj ) {
+  var name = newObj.name;
+  var id = newObj.id;
+  this.genre = newObj.genre;
+  this.image = newObj.image;
+  this.bio = newObj.bio;
+  
+  this.getId = function() {
+    return id;
+  }
+  this.getName = function() {
+    return name;
+  }
+  this.setName = function( newName ) {
+    name = newName;
+    domUpdateName();
+  }
+  this.blurb = function() {
+    console.log( this.getName() + ' played ' + this.genre );
+  };
+
+  function domUpdateName() {
+    var playerEl = document.getElementById( id );
+    var nameEl = playerEl.getElementsByTagName('h2')[0];
+    nameEl.innerHTML = name;
+    console.log( 'keedl', playerEl );
+  }
+}
