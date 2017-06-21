215b0ed99d7ad66879a76aa4edbbb21acc1a0252
Elias Carlston <elias@eliascarlston.com>
2017-06-21 11:17:25 -0400

    Add player JS example

---
+++ intro-to-web-dev/examples/day2/02-js/players/01-players.js
@@ -0,0 +1,17 @@
+var players = [
+  {
+    'name'  : 'Miles Davis',
+    'image' : 'http://vignette1.wikia.nocookie.net/marvelcinematicuniverse/images/e/e8/Miles_Davis.jpeg/revision/latest?cb=20161003223700',
+    'bio'   : 'Miles Dewey Davis III was an American jazz trumpeter, bandleader, and composer. He is among the most influential and acclaimed figures in the history of jazz and 20th century music.'
+  },
+  {
+    'name': 'Willie Nelson',
+    'image' : 'http://images.musictimes.com/data/images/full/1337/willie-nelson-studies-deeply-before-picking-tour-openers.jpg',
+    'bio'   : 'Willie Hugh Nelson is an American musician, singer, songwriter, author, poet, actor, and activist. The critical success of the album Shotgun Willie, combined with the critical and commercial success of Red Headed Stranger and Stardust, made Nelson one of the most recognized artists in country music.'
+  },
+  {
+    'name': 'Prince',
+    'image' : 'https://i.ytimg.com/vi/7NN3gsSf-Ys/maxresdefault.jpg',
+    'bio'   : 'Prince Rogers Nelson was an American singer-songwriter, actor, multi-instrumentalist, philanthropist, dancer and record producer. He was a musical innovator who was known for his eclectic work, flamboyant stage presence, extravagant dress and makeup, and wide vocal range.'
+  }
+];
