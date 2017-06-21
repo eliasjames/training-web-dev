369619821bb6a4dc419c07c3a3c31270aa8e0aee
Elias Carlston <elias@eliascarlston.com>
2017-06-21 16:04:06 -0400

    Add DOM Examples

---
+++ intro-to-web-dev/examples/day3/pm-js/02-events/02-player-constructor.js
@@ -0,0 +1,42 @@
+function Player( newObj ) {
+  var playerEl, nameEl; 
+
+  var name = newObj.name;
+  var id = newObj.id;
+  this.genre = newObj.genre;
+  this.image = newObj.image;
+  this.bio = newObj.bio;
+  
+  this.getId = function() {
+    return id;
+  }
+  this.getName = function() {
+    return name;
+  }
+  this.setName = setName;
+
+  this.blurb = function() {
+    console.log( this.getName() + ' played ' + this.genre );
+  };
+
+  function domGetNodes() {
+    playerEl = document.getElementById( id );
+    nameEl = playerEl.getElementsByTagName( 'h2' )[0];
+    inputEl = playerEl.getElementsByTagName( 'input' )[0];
+    inputEl.addEventListener( 'change', function( event ) {
+      setName( event.target.value );
+    });
+  }
+  function domUpdateName() {
+    nameEl.innerHTML = name;
+    inputEl.value = name;
+    console.log( 'keedl', playerEl );
+  }
+  function setName ( newName ) {
+    name = newName;
+    if ( !playerEl || !nameEl ) {
+      domGetNodes();
+    }
+    domUpdateName();
+  }
+}
