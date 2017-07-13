6f97ea935fb41675fd488d9f84b4185f65da757d
Elias Carlston <elias@eliascarlston.com>
2017-07-12 21:07:37 -0400

    Remove unneeded files

--- intermediate-front-end/examples/day2/pm-sass/00-require-jquery.js
+++
@@ -1,3 +0,0 @@
-define( [ './node_modules/jquery/dist/jquery.js' ], function() {
-  return jQuery;
-});
--- intermediate-front-end/examples/day2/pm-sass/01-controls.js
+++
@@ -1,8 +0,0 @@
-define( [ '01-config' ], function( GLOBAL_CONFIG ) {
-  $( document ).ready(function() {
-      $( '#' + GLOBAL_CONFIG.ui.els.newGame )
-        .on( 'click', ()=>{
-          document.dispatchEvent( new Event( 'startNewGame' ));
-        })
-  });
-});
